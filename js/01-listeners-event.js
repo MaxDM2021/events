bb9d02f4b095a34bfe7f071aeefa19550c29d773
Maksym Dmitriiev <maximdm22@gmail.com>
2022-07-17 21:58:42 +0300

    111

---
+++ js/01-listeners-event.js
@@ -0,0 +1,13 @@
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const numberOfCat = document.querySelector('#item')
+console.log(numberOfCat);
